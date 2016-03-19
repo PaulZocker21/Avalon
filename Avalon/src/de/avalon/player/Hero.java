afd1431a7bf060c79e64955df472e278acb55e3c
Paul <paulzocker21@googlemail.com>
2016-03-19 18:51:01 +0100

    Hero class added

---
+++ Avalon/src/de/avalon/player/Hero.java
@@ -0,0 +1,36 @@
+package de.avalon.player;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.bukkit.entity.Player;
+
+public class Hero {
+
+	private static HashMap<UUID, Hero> heros = new HashMap<>();
+
+	private UUID uuid;
+	private String name;
+
+	private Hero(UUID uuid, String name) {
+		this.uuid = uuid;
+		this.name = name;
+	}
+
+	public static Hero create(Player player) {
+
+	}
+
+	public UUID getUniqueId() {
+		return uuid;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
